857d37f5f48c224ab2c136122be5bf4a423dd09a
EmrayP <emraypala@gmail.com>
2020-11-23 20:55:14 -0600

    creating the new project

---
+++ src/main/java/com/ticketingappmvc/TicketingAppMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingappmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingAppMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingAppMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketingappmvc/TicketingAppMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketingappmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingAppMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
